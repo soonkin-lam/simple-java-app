8417cb00cc592b6316c6bdf5dae0827a78e0c702
SK Lam <lam_4jan1987@hotmail.com>
2024-02-21 09:24:27 +0800

    Test webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello SK ! I am simple Java app");
+        System.out.println("Hello SK !! I am simple Java app");
     }
 
     public String getMessage() {
